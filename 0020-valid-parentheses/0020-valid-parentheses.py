250ca26792152ff03c7a56dc00ba7fbbc9c19d22
Sankalp Gunturi <sgunturi@protonmail.com>
2023-08-26 12:12:21 -0700

    Time: 43 ms (54.52%), Space: 16.4 MB (51.3%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -2,11 +2,10 @@ class Solution:
     def isValid(self, s: str) -> bool:
         if len(s) < 2:
             return False
-        openB = ('{', '(', '[')
         closeToOpen = {'}':'{', ')':'(', ']':'['}
         stack = []
         for c in s:
-            if c in openB:
+            if c in closeToOpen.values():
                 stack.append(c)
             else:
                 if stack and stack[-1] == closeToOpen[c]:
