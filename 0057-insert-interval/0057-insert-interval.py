9973fdd7a46fb13ee9ef41ea947ea0fbf7f25182
Sankalp Gunturi <sgunturi@protonmail.com>
2023-09-01 15:51:43 -0700

    Time: 113 ms (5.48%), Space: 19.9 MB (42.62%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,19 +1,27 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        if len(intervals) == 0:
+            return [newInterval]
         output = []
-        for i in range(len(intervals)):
-            # Comes before the interval
-            if newInterval[1] < intervals[i][0]:
+        flag = False
+        for i, interval in enumerate(intervals):
+            if interval[0] > newInterval[0] and flag == False:
                 output.append(newInterval)
-                return output + intervals[i:]
+                flag = True
+            output.append(interval)
+        if len(output) == len(intervals):
+            output.append(newInterval)
 
-            # Comes after the interval
-            elif newInterval[0] > intervals[i][1]:
-                output.append(intervals[i])
+        i = 0
+        while i < len(output) - 1:
+            currEnd, nextStart = output[i][1], output[i + 1][0]
+            currStart, nextEnd = output[i][0], output[i + 1][1]
+            if currEnd >= nextStart:
+                output.pop(i)
+                output.pop(i)
+                output.insert(i, [min(currStart, nextStart), max(currEnd, nextEnd)])
+                i = 0
+                continue
+            i += 1
 
-            # Overlaps with the interval
-            else:
-                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
-
-        output.append(newInterval)
         return output
